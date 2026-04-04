9787e6d147ce932e7ee94888eb6215fc19c63881
Harshil Patel <harshilp046@gmail.com>
2026-04-05 02:15:03 +0530

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function isValidUrl(url: string) {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
+  return twMerge(clsx(inputs))
 }
